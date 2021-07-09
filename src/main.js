dcf4c815077559bb927a384298483eb307a62744
Ajuro Takahashi <ajuro.takahashi@gmail.com>
2021-07-09 14:20:43 +0200

    offline

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './index.css'
+import './index.css';
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// Register the service worker
+if ('serviceWorker' in navigator) {
+    // Wait for the 'load' event to not block other work
+    window.addEventListener('load', async () => {
+      // Try to register the service worker.
+      try {
+        const reg = await navigator.serviceWorker.register('./sw.js');
+        console.log('Service worker registered! :sunglasses:', reg);
+      } catch (err) {
+        console.log(':disappointed_relieved: Service worker registration failed: ', err);
+      }
+    });
+  }
